ded642f02f7bd44488a28b6aed2684b691082593
AhmedGamal97 <ag469389@gmail.com>
2023-04-03 12:58:27 +0200

    create styles file,title best seller

--- lib/features/Home/presentation/views/widget/custom_app_bar.dart
+++ lib/features/Home/presentation/views/widget/custom_app_bar.dart
@@ -8,25 +8,20 @@ class CustomAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 24.0,
-      ),
-      child: Row(
-        children: [
-          Image.asset(
-            AssetsData.logo,
-            height: 18,
-          ),
-          const Spacer(),
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                FontAwesomeIcons.magnifyingGlass,
-                size: 24,
-              ))
-        ],
-      ),
+    return Row(
+      children: [
+        Image.asset(
+          AssetsData.logo,
+          height: 18,
+        ),
+        const Spacer(),
+        IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              FontAwesomeIcons.magnifyingGlass,
+              size: 24,
+            ))
+      ],
     );
   }
 }
--- lib/features/Home/presentation/views/widget/home_view_body.dart
+++ lib/features/Home/presentation/views/widget/home_view_body.dart
@@ -1,34 +1,29 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../core/uitls/styles.dart';
 import 'custom_app_bar.dart';
-import 'custom_listview_item.dart';
+import 'feature_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const [CustomAppBar(), FeaturedBooksListView()],
-    );
-  }
-}
-
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (BuildContext context, int index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0),
-            child: FeaturedListViewItem(),
-          );
-        },
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'Best Seller',
+            style: Styles.titleMedium,
+          ),
+        ],
       ),
     );
   }
