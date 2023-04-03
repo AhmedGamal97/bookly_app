1e1a8c608b60aa4fa470cd16707ce7ebc3da5351
AhmedGamal97 <ag469389@gmail.com>
2023-04-03 12:03:18 +0200

    add custom list view item to home body

--- lib/features/Home/presentation/views/widget/home_view_body.dart
+++ lib/features/Home/presentation/views/widget/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
+import 'custom_listview_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -8,7 +9,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [CustomAppBar()],
+      children: const [CustomAppBar(), CustomListViewItem()],
     );
   }
 }
