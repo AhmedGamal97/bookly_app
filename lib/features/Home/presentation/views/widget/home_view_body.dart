586cc4a3a3c3d7157a668ca26a40e685c9cdc247
AhmedGamal97 <ag469389@gmail.com>
2023-04-04 14:06:07 +0200

    best seller item p2

--- lib/features/Home/presentation/views/widget/home_view_body.dart
+++ lib/features/Home/presentation/views/widget/home_view_body.dart
@@ -1,4 +1,3 @@
-import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/uitls/assets.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +11,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      padding: const EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: const [
@@ -24,6 +23,9 @@ class HomeViewBody extends StatelessWidget {
           Text(
             'Best Seller',
             style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 20,
           ),
           BestSellerListViewItem()
         ],
@@ -51,7 +53,22 @@ class BestSellerListViewItem extends StatelessWidget {
                       fit: BoxFit.fill, image: AssetImage(AssetsData.test))),
             ),
           ),
-          Column()
+          const SizedBox(
+            width: 30,
+          ),
+          Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: const Text(
+                  'Harry Potter and the Goblet of Fire',
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Styles.textStyle20,
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
