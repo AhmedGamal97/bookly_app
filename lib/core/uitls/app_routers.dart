8b87982f234a7c74997100ea1a4f9a611560fec6
AhmedGamal97 <ag469389@gmail.com>
2023-04-05 16:08:14 +0200

    create searvh view

--- lib/core/uitls/app_routers.dart
+++ lib/core/uitls/app_routers.dart
@@ -1,5 +1,6 @@
 import 'package:bookly_app/features/Home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/features/Home/presentation/views/home_view.dart';
+import 'package:bookly_app/features/search/presentation/views/search.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../features/Splash/presentation/views/splash_view.dart';
@@ -7,6 +8,7 @@ import '../../features/Splash/presentation/views/splash_view.dart';
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kBookDetailsView = '/BookDetailsView';
+  static const kSearchView = '/SearchView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -20,6 +22,10 @@ abstract class AppRouter {
       GoRoute(
         path: kBookDetailsView,
         builder: (context, state) => const BookDetailsView(),
+      ),
+      GoRoute(
+        path: kBookDetailsView,
+        builder: (context, state) => const SearchView(),
       ),
     ],
   );
