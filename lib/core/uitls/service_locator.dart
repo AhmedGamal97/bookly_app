5efef5ab384ea411d954aca4affa6a929f003dcb
AhmedGamal97 <ag469389@gmail.com>
2023-04-06 14:07:40 +0200

    setup service locator

---
+++ lib/core/uitls/service_locator.dart
@@ -0,0 +1,11 @@
+import 'package:bookly_app/features/Home/data/repo/home_repo_imp.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+import 'api_service.dart';
+
+final getIt = GetIt.instance;
+void setupServiceLocator() {
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(ApiService(Dio())));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:bookly_app/core/uitls/api_service.dart';
+import 'package:bookly_app/features/Home/data/repo/home_repo_imp.dart';
+import 'package:bookly_app/features/Home/presentation/manager/Newest%20Books%20cubit/newest_books_cubit.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'constants.dart';
 import 'core/uitls/app_routers.dart';
+import 'core/uitls/service_locator.dart';
+import 'features/Home/presentation/manager/Featurd Books cubit/featurd_books_cubit.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
@@ -13,14 +21,24 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: kPrimaryColor,
-          textTheme:
-              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
-      //home: const SplashView(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturdBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(getIt.get<HomeRepoImpl>()),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: kPrimaryColor,
+            textTheme:
+                GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
+        //home: const SplashView(),
+      ),
     );
   }
 }
