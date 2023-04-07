0e59386f0c1510021899480ecb4ed7d74fd89876
AhmedGamal97 <ag469389@gmail.com>
2023-04-07 19:39:00 +0200

    book details view done

--- lib/core/widgets/custom_button.dart
+++ lib/core/widgets/custom_button.dart
@@ -9,19 +9,20 @@ class CutomButton extends StatelessWidget {
     this.borderRadius,
     required this.text,
     this.fontSize,
+    this.onPressed,
   });
   final String text;
   final Color backGroundColor;
   final Color textColor;
   final double? fontSize;
   final BorderRadius? borderRadius;
-
+  final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 48,
       child: TextButton(
-        onPressed: () {},
+        onPressed: onPressed,
         style: TextButton.styleFrom(
             backgroundColor: backGroundColor,
             shape: RoundedRectangleBorder(
--- lib/features/Home/presentation/views/widget/book_action.dart
+++ lib/features/Home/presentation/views/widget/book_action.dart
@@ -1,39 +1,59 @@
+import 'package:bookly_app/core/uitls/Function/launch_url.dart';
 import 'package:flutter/material.dart';
+
+import 'package:bookly_app/features/Home/data/models/book_model/book_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../../core/widgets/custom_button.dart';
 
 class BooksAction extends StatelessWidget {
-  const BooksAction({super.key});
-
+  const BooksAction({
+    Key? key,
+    required this.bookModel,
+  }) : super(key: key);
+  final BookModel bookModel;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8),
       child: Row(
-        children: const [
-          Expanded(
-              child: CutomButton(
-            backGroundColor: Colors.white,
-            textColor: Colors.black,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(16),
-              bottomLeft: Radius.circular(16),
+        children: [
+          const Expanded(
+            child: CutomButton(
+              backGroundColor: Colors.white,
+              textColor: Colors.black,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16),
+                bottomLeft: Radius.circular(16),
+              ),
+              text: 'Free',
             ),
-            text: 'Free',
-          )),
+          ),
           Expanded(
-              child: CutomButton(
-            backGroundColor: Color(0xffEF8262),
-            fontSize: 16,
-            textColor: Colors.white,
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(16),
-              bottomRight: Radius.circular(16),
+            child: CutomButton(
+              onPressed: () async {
+                launchCustomUrl(context, bookModel.volumeInfo.previewLink!);
+              },
+              backGroundColor: const Color(0xffEF8262),
+              fontSize: 16,
+              textColor: Colors.white,
+              borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(16),
+                bottomRight: Radius.circular(16),
+              ),
+              text: getText(bookModel),
             ),
-            text: 'Preview',
-          )),
+          ),
         ],
       ),
     );
+  }
+
+  String getText(BookModel bookModel) {
+    if (bookModel.volumeInfo.previewLink == null) {
+      return 'Not Avalible';
+    } else {
+      return 'Preview';
+    }
   }
 }
